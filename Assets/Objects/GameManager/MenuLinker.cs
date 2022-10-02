acf0fe40a920bc49165043f3acf4af8a1985c65b
LucaSchifano <luca.schifano005@gmail.com>
2022-10-02 16:42:43 +0200

    Add ui and menu

--- Assets/Objects/GameManager/MenuLinker.cs
+++ Assets/Objects/GameManager/MenuLinker.cs
@@ -2,19 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class MenuLinker : MonoBehaviour
 {
     [SerializeField] Button startButton;
+    [SerializeField] Button optionButton;
+    [SerializeField] Button quitButton;
+
+    [SerializeField] private GameObject mainScreen;
+    [SerializeField] private GameObject optionScreen;
+
+    [SerializeField] private AudioMixer audioMixer;
+
+    private bool isOption;
 
     void Start()
     {
         startButton.onClick.RemoveAllListeners();
+        quitButton.onClick.RemoveAllListeners();
         startButton.onClick.AddListener(GameManager.Instance.CallStartPlay);
+        quitButton.onClick.AddListener(GameManager.Instance.CallQuit);
+    }
+    
+    public void OptionsChangeScreen()
+    {
+        if (isOption)
+        {
+            optionScreen.SetActive(false);
+            mainScreen.SetActive(true);
+        }
+        else
+        {
+            optionScreen.SetActive(true);
+            mainScreen.SetActive(false);
+        }
+        
+        isOption = !isOption;
     }
 
-    public void loadscne()
+    public void changeOSTSlider(float lefloat)
     {
-        GameManager.Instance.CallStartPlay();
+        audioMixer.SetFloat("OST", Mathf.Log10(lefloat) * 20);
     }
+    
+    public void changeSFXSlider(float lefloat)
+    {
+        audioMixer.SetFloat("SFX", Mathf.Log10(lefloat) * 20);
+    }
+    
 }
