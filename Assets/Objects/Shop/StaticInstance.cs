a7aef6ba2ba6c2f1bddd6c1e3b6656599d0491e5
MrEsport <lucasespositonc@gmail.com>
2022-10-02 12:01:23 +0200

    Fixed Destroy instance bug

--- Assets/Objects/Shop/StaticInstance.cs
+++ Assets/Objects/Shop/StaticInstance.cs
@@ -2,31 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-    /// <summary>
-    /// A static instance is similar to a singleton, but instead of destroying any new
-    /// instances, it overrides the current instance. This is handy for resetting the state
-    /// and saves you doing it manually
-    /// </summary>
-    public abstract class StaticInstance<T> : MonoBehaviour where T : MonoBehaviour {
-        public static T Instance { get; private set; }
-        protected virtual void Awake() => Instance = this as T;
+/// <summary>
+/// A static instance is similar to a singleton, but instead of destroying any new
+/// instances, it overrides the current instance. This is handy for resetting the state
+/// and saves you doing it manually
+/// </summary>
+public abstract class StaticInstance<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance { get; private set; }
 
-        protected virtual void OnApplicationQuit() {
-            Instance = null;
+    protected virtual void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.Log($"<color=red>FOUND INSTANCE IN {Instance.gameObject.name}, DESTROYING SELF {gameObject.name}</color>");
             Destroy(gameObject);
+            return;
         }
+        Instance = this as T;
     }
 
-    /// <summary>
-    /// This transforms the static instance into a basic singleton. This will destroy any new
-    /// versions created, leaving the original instance intact
-    /// </summary>
-    public abstract class Singleton<T> : StaticInstance<T> where T : MonoBehaviour {
-        protected override void Awake() {
-            if (Instance != null) Destroy(gameObject);
-            base.Awake();
-        }
+    protected virtual void OnApplicationQuit()
+    {
+        Instance = null;
+        Destroy(gameObject);
     }
+}
+
+/// <summary>
+/// This transforms the static instance into a basic singleton. This will destroy any new
+/// versions created, leaving the original instance intact
+/// </summary>
+public abstract class Singleton<T> : StaticInstance<T> where T : MonoBehaviour
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+}
 
 /// <summary>
 /// Finally we have a persistent version of the singleton. This will survive through scene
